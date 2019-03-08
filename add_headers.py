fdec526ced852935144fc2cefa804164d3d053b5
Jacky Poon <jackypoon0@gmail.com>
2019-03-08 20:57:35 +1100

    Add headers
    Make notebooks consistent with the theme.

--- add_headers.py
+++ add_headers.py
@@ -1,1 +1,44 @@
-# Iterates over html files and adds header
+""" Iterates over html files and adds header """
+import glob
+import json
+import re
+import os
+import time
+
+for notebook_file in glob.glob("content/posts/*.ipynb"):
+    # Get details:
+    with open(notebook_file, "r") as f:
+        notebook_json = json.load(f)
+
+        pattern = re.compile("ipynb", re.IGNORECASE)
+        title = pattern.sub("", os.path.basename(f.name))
+        date = time.strftime(
+            "%Y-%m-%dT%H:%M:%S+00:00",
+            time.gmtime(
+                os.path.getmtime(notebook_file)
+            )
+        )
+        description = "/n".join(notebook_json["cells"][0]["source"])
+        f.close()
+
+    # Make template
+    header_template = f"""+++
+title =  "{title}"
+date = {date}
+tags = []
+featured_image = ""
+description = "{description}"
++++"""
+
+    pattern = re.compile("ipynb", re.IGNORECASE)
+
+    html_file = pattern.sub("html", notebook_file)
+
+    print(html_file)
+
+    with open(html_file, "r+") as f:
+        contents = f.read()
+        f.seek(0)
+        f.write(header_template + contents)
+        f.truncate()
+        f.close()
